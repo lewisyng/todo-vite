095ffcd2525a886b4223d576cb42febdf35589c1
Lewis Young <lewisyoung1306@yahoo.de>
2023-02-19 00:40:25 +0100

    minor styling fixes

--- src/components/UI/Molecules/BoardList/BoardList.tsx
+++ src/components/UI/Molecules/BoardList/BoardList.tsx
@@ -1,7 +1,8 @@
 import { BoardTile } from '@src/components/UI/Atoms/BoardTile/BoardTile';
 import { Board } from '@src/models';
+import cn from 'classnames';
 import { FunctionComponent } from 'react';
-import styles from "./BoardList.module.scss"
+import styles from './BoardList.module.scss';
 
 type BoardListProps = {
     boards: Board[];
@@ -9,11 +10,8 @@ type BoardListProps = {
 
 export const BoardList: FunctionComponent<BoardListProps> = ({ boards }) => {
     return (
-        <div className={styles["board-list"]}>
-            {boards &&
-                boards.map((board: Board, idx: number) => (
-                    <BoardTile key={idx} board={board} />
-                ))}
+        <div className={cn(styles['board-list'], boards.length < 3 && styles['board-list--flex'])}>
+            {boards && boards.map((board: Board, idx: number) => <BoardTile key={idx} board={board} />)}
         </div>
     );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Typography from '@src/components/UI/Atoms/Typography/Typography';
 import { BoardList } from '@src/components/UI/Molecules/BoardList/BoardList';
 import { database } from '@src/database';
 import { useLiveQuery } from 'dexie-react-hooks';
@@ -6,12 +7,12 @@ import styles from './Home.module.scss';
 export const Home = () => {
     const boards = useLiveQuery(() => database.boards.toArray());
 
-    console.log('boards', boards);
-
     return (
         <div className={styles['home__wrapper']}>
             <div className={styles['home']}>
-                <h1>Select one of your todo boards</h1>
+                <Typography as="h1" weight="bold" size="text-lg" className={styles['home__header']}>
+                    Select a board
+                </Typography>
 
                 {boards && <BoardList boards={boards} />}
             </div>
